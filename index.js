d4999908fe4fc3966ba9e31e69a3041f09ecc98f
MarkSkinner92 <markhskinner@gmail.com>
2024-07-25 13:02:13 -0600

    fixed scanning forever issue

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ io.on('connection', (socket) => {
                 rovManager.sendFullStateTo(io);
             },()=>{
                 //scan is done
+                rovManager.sendFullStateTo(io);
             });
         }
 
